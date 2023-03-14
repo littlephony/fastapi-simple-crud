4ee0f2a121a26969a13bfc3c39d6dc54929a5a15
littlephony <uladzislau.lukashevich@gmail.com>
2023-03-14 17:05:46 +0100

    Included router

--- src/app/app.py
+++ src/app/app.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
+from router import router
 
 app = FastAPI()
 
 @app.get("/")
 async def hello() -> dict:
     return { "message": "Hello, World!"}
+
+app.include_router(router)
