124ddc2ac1c5987314394f9b2eb63c2971295ddc
littlephony <uladzislau.lukashevich@gmail.com>
2023-03-14 16:45:01 +0100

    Added GET method

--- src/app/router.py
+++ src/app/router.py
@@ -10,3 +10,21 @@ async def add_todo(todo: Todo) -> dict:
     todos.append(todo)
     return {"message": "Todo added successfully."}
 
+@router.get("/todo")
+async def retrieve_todos() -> dict:
+    return {"todos": todos}
+
+@router.get("/todo/{todo_id}")
+async def get_todo(
+    todo_id: int = Path(
+        ...,
+        title="ID of todo to retrieve"
+    )
+) -> dict:
+    for todo in todos:
+        if todo.id == todo_id:
+            return {"todo": todo}
+    return {"message": "Todo with provided ID doesn't exist"}
+
+    
+    
